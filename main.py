d2dc532d338075aab3b47b7c1fc6effb24dc3a81
diceworld <diceworld@naver.com>
2022-10-01 19:40:38 +0900

    Query 를 이용한 입력갑 제한

--- main.py
+++ main.py
@@ -1,23 +1,40 @@
-from typing import Optional
+from typing import Optional, Union, List
 
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, Query
+from pydantic import Required
 
 app = FastAPI()
 
 
-class Item(BaseModel):
-    name: str
-    description: Optional[str] = None
-    price: float
-    tax: Optional[float] = None
-
-
-@app.post("/items/{item_id}")
-async def create_item(item_id: int, item: Item, q: Optional[str] = None):
-    result = {"item_id": item_id, **item.dict()}
+@app.get("/item/")
+async def read_item(q: Optional[str] = Query(default=Required, min_length=3, max_length=50, regex="[a-zA-Z]")):
+    results = {"item": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
 
     if q:
-        result.update({"q": q})
+        results.update({"q": q})
 
-    return result
+    return results
+
+
+@app.get("/items/")
+async def read_items(q: Union[List[str], None] = Query(default=["foo", "bar"])):
+    query_items = {"q": q}
+    return query_items
+
+
+@app.get("/goods/")
+async def read_goods(
+        q: Optional[str] = Query(
+            default=None,
+            alias="goods-query",
+            title="Query string",
+            description="Query String for the items to search in the database that have a good match",
+            min_length=3,
+        )
+):
+    results = {"goods": [{"goods_id": "foo"}, {"goods_id": "bar"}]}
+
+    if q:
+        results.update({"q": q})
+
+    return results

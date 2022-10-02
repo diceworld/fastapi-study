c15421afa97a4b30f7ba68c511f74932125b3329
diceworld <diceworld@naver.com>
2022-10-02 19:27:53 +0900

    Body 중첩 모델 입력값 처리

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
-from typing import Optional
+from typing import Optional, List
 
 from fastapi import FastAPI, Body
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, HttpUrl
 
 app = FastAPI()
+
+
+class Image(BaseModel):
+    url: HttpUrl
+    name: str
 
 
 class Item(BaseModel):
     name: str
     description: Optional[str] = Field(default=None, title="description", max_length=30)
+    tags: List[str] = []
+    image: Optional[List[Image]] = None
 
 
 @app.put("/items/{item_id}")
 async def update_item(
         *,
         item_id: int,
-        item: Item = Body(embed=True),
-        q: Optional[str] = None
+        item: Item = Body(embed=True)
 ):
     results = {"item_id": item_id, "item": item}
-
-    if q:
-        results.update({"q": q})
 
     return results

e13ed8494aa94a4e69c67cffbef2601a8ba6eb0d
diceworld <diceworld@naver.com>
2022-10-02 11:10:06 +0900

    경로 매개변수와 숫자검증

--- main.py
+++ main.py
@@ -1,39 +1,15 @@
-from typing import Optional, Union, List
-
-from fastapi import FastAPI, Query
-from pydantic import Required
+from fastapi import FastAPI, Path, Query
 
 app = FastAPI()
 
 
-@app.get("/item/")
-async def read_item(q: Optional[str] = Query(default=Required, min_length=3, max_length=50, regex="[a-zA-Z]")):
-    results = {"item": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-
-    if q:
-        results.update({"q": q})
-
-    return results
-
-
-@app.get("/items/")
-async def read_items(q: Union[List[str], None] = Query(default=["foo", "bar"])):
-    query_items = {"q": q}
-    return query_items
-
-
-@app.get("/goods/")
-async def read_goods(
-        q: Optional[str] = Query(
-            default=None,
-            alias="goods-query",
-            title="Query string",
-            description="Query String for the items to search in the database that have a good match",
-            min_length=3,
-        )
+@app.get("/items/{item_id}")
+async def read_items(
+        *, item_id: int = Path(title="The ID of the item to get", gt=0, le=1000),
+        q: str,
+        size: float = Query(gt=0, lt=10.5)
 ):
-    results = {"goods": [{"goods_id": "foo"}, {"goods_id": "bar"}]}
-
+    results = {"item_id": item_id}
     if q:
         results.update({"q": q})
 

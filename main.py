2cf99713565d2adf4ffec7df2f41b8d5052856ce
diceworld <diceworld@naver.com>
2022-10-02 19:49:25 +0900

    BaseModel 에 샘플 값 설정

--- main.py
+++ main.py
@@ -6,24 +6,32 @@ from pydantic import BaseModel, Field, HttpUrl
 app = FastAPI()
 
 
-class Image(BaseModel):
-    url: HttpUrl
-    name: str
-
-
 class Item(BaseModel):
     name: str
-    description: Optional[str] = Field(default=None, title="description", max_length=30)
-    tags: List[str] = []
-    image: Optional[List[Image]] = None
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Foo",
+                "description": "A very nice Item",
+                "price": 35.4,
+                "tax": 3.2,
+            }
+        }
+
+
+class Item2(BaseModel):
+    name: str = Field(example="Foo")
+    description: Optional[str] = Field(default=None, example="A very nice Item")
+    price: float = Field(example=35.4)
+    tax: Optional[float] = Field(default=None, example=3.2)
 
 
 @app.put("/items/{item_id}")
-async def update_item(
-        *,
-        item_id: int,
-        item: Item = Body(embed=True)
-):
-    results = {"item_id": item_id, "item": item}
+async def update_item(item_id: int, item: Item, item2: Item2):
+    results = {"item_id": item_id, "item": item, "item2": item2}
 
     return results

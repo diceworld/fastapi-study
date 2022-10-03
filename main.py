0d015e1131f66ff53c29993a07dded3c6442c0bf
diceworld <diceworld@naver.com>
2022-10-03 10:53:58 +0900

    Cookie and Header Parameters

--- main.py
+++ main.py
@@ -1,37 +1,13 @@
-import uuid
-from datetime import datetime, time, timedelta
 from typing import Optional
-from uuid import UUID
 
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Cookie, Header
 
 app = FastAPI()
 
 
-@app.get("/uuid")
-async def get_uuid():
-    return {
-        "uuid": uuid.uuid1(),
-    }
-
-
-@app.put("/items/{item_id}")
+@app.get("/items/")
 async def read_items(
-        item_id: UUID,
-        start_datetime: Optional[datetime] = Body(default=None),
-        end_datetime: Optional[datetime] = Body(default=None),
-        repeat_at: Optional[time] = Body(default=None),
-        process_after: Optional[timedelta] = Body(default=None)
+        ads_id: Optional[str] = Cookie(default=None),
+        user_agent: Optional[str] = Header(default=None)
 ):
-    start_process = start_datetime + process_after
-    duration = end_datetime - start_process
-
-    return {
-        "item_id": item_id,
-        "start_datetime": start_datetime,
-        "end_datetime": end_datetime,
-        "repeat_at": repeat_at,
-        "precess_after": process_after,
-        "start_process": start_process,
-        "duration": duration
-    }
+    return {"ads_id": ads_id, "user_agent": user_agent}

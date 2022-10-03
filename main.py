057fbe72d043b37fc38361fb2027301c262d63c7
diceworld <diceworld@naver.com>
2022-10-03 11:18:50 +0900

    Response Model

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
-from typing import Optional
+from typing import Optional, List
 
-from fastapi import FastAPI, Cookie, Header
+from fastapi import FastAPI
+from pydantic import EmailStr
+from pydantic.main import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/items/")
-async def read_items(
-        ads_id: Optional[str] = Cookie(default=None),
-        user_agent: Optional[str] = Header(default=None)
-):
-    return {"ads_id": ads_id, "user_agent": user_agent}
+class UserIn(BaseModel):
+    username: str
+    password: str
+    email: EmailStr
+    full_name: Optional[str] = None
+
+
+class UserOut(BaseModel):
+    username: str
+    email: EmailStr
+    full_name: Optional[str] = None
+
+
+@app.post("/usr/", response_model=UserOut)
+async def create_user(user: UserIn):
+    return user

7f69759feac199e70f22da40191c96ae72b11985
diceworld <diceworld@naver.com>
2022-10-03 14:09:38 +0900

    body put

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from datetime import datetime
-from typing import Optional
+from typing import Optional, Union, List
 
 from fastapi import FastAPI
 from fastapi.encoders import jsonable_encoder
@@ -7,17 +6,29 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-fake_db = {}
-
 
 class Item(BaseModel):
-    title: str
-    timestamp: datetime
-    description: Optional[str] = None
+    name: Optional[str] = None
+    description: Union[str, None] = None
+    price: Union[float, None] = None
+    tax: float = 10.5
+    tags: List[str] = []
 
 
-@app.put("/items/{id}")
-async def update_item(id: str, item: Item):
-    json_compatible_item_data = jsonable_encoder(item)
-    fake_db[id] = json_compatible_item_data
-    return fake_db[id]
+items = {
+    "foo": {"name": "Foo", "price": 50.2},
+    "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
+    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
+}
+
+
+@app.get("/items/{item_id}", response_model=Item)
+async def read_item(item_id: str):
+    return items[item_id]
+
+
+@app.put("/items/{item_id}", response_model=Item)
+async def update_item(item_id: str, item: Item):
+    update_item_encoded = jsonable_encoder(item)
+    items[item_id] = update_item_encoded
+    return update_item_encoded

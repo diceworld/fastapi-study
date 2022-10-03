e57779a33e680ecc225f0bb696c987dde4f211ae
diceworld <diceworld@naver.com>
2022-10-03 14:50:01 +0900

    미들웨어

--- main.py
+++ main.py
@@ -1,30 +1,19 @@
-from typing import Union
+import time
 
-from fastapi import Depends, FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 
-async def common_parameters(
-        q: Union[str, None] = None,
-        skip: int = 0,
-        limit: int = 100
-):
-    return {"q": q, "skip": skip, "limit": limit}
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
 
 
-class CommonQueryParams:
-    def __init__(self, q: Union[str, None] = None, skip: int = 0, limit: int = 100):
-        self.q = q
-        self.skip = skip
-        self.limit = limit
-
-
-@app.get("/items/")
-async def read_items(commons: dict = Depends(common_parameters)):
-    return commons
-
-
-@app.get("/users/")
-async def read_users(commons: CommonQueryParams = Depends(CommonQueryParams)):
-    return commons
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}

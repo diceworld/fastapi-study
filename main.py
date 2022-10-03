12f847060448d28ce298bf690de76cffcf57c08c
diceworld <diceworld@naver.com>
2022-10-03 13:48:39 +0900

    JSON 호환 가능 인코더

--- main.py
+++ main.py
@@ -1,44 +1,23 @@
-from enum import Enum
-from typing import Union, Set
+from datetime import datetime
+from typing import Optional
 
 from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
 
 app = FastAPI()
 
-
-class Tags(Enum):
-    items = "items"
-    users = "users"
+fake_db = {}
 
 
 class Item(BaseModel):
-    name: str
-    description: Union[str, None] = None
-    price: float
-    tax: Union[float, None] = None
-    tags: Set[str] = set()
+    title: str
+    timestamp: datetime
+    description: Optional[str] = None
 
 
-@app.post(
-    "/items/",
-    response_model=Item,
-    summary="Create an item",
-    tags=[Tags.items]
-)
-async def create_item(item: Item):
-    """
-    Create an item with all the information:
-
-    - **name**: each item must have a name
-    - **description**: a long description
-    - **price**: required
-    - **tax**: if the item doesn't have tax, you can omit this
-    - **tags**: a set of unique tag strings for this item
-    """
-    return item
-
-
-@app.get("/users/", tags=[Tags.users])
-async def read_users():
-    return ["Rick", "Morty"]
+@app.put("/items/{id}")
+async def update_item(id: str, item: Item):
+    json_compatible_item_data = jsonable_encoder(item)
+    fake_db[id] = json_compatible_item_data
+    return fake_db[id]

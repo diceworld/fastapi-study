928f1a0213f3cac3c8fe685c4f615f3ea3abca65
diceworld <diceworld@naver.com>
2022-10-12 09:51:09 +0900

    metadata

---
+++ app/routers/metadata.py
@@ -0,0 +1,61 @@
+from fastapi import FastAPI
+
+description = """
+ChimichangApp API helps you do awesome stuff.
+
+## Items
+
+You can **read items**.
+
+## Users
+
+You will be able to:
+
+* **Create users** (_not implemented_).
+* **Read users** (_not implemented_).
+"""
+
+tags_metadata = [
+    {
+        "name": "users",
+        "description": "Operations with users. The **login** logic is also here.",
+    },
+    {
+        "name": "items",
+        "description": "Manage items. So _fancy_ they have their own docs.",
+        "externalDocs": {
+            "description": "Items external docs",
+            "url": "https://fastapi.tiangolo.com/",
+        },
+    },
+]
+
+app = FastAPI(
+    title="ChimichangApp",
+    description=description,
+    version="0.0.1",
+    terms_of_service="http://example.com/terms/",
+    contact={
+        "name": "Deadpool is the Amazing",
+        "url": "http://x-force.example.com/contact/",
+        "email": "dp@x-force.example.com"
+    },
+    license_info={
+        "name": "Apache 2.0",
+        "url": "https://www.apache.org/licenses/LICENSE-2.0.html"
+    },
+    openapi_tags=tags_metadata,
+    openapi_url="/api/v1/openapi.json",
+    docs_url="/documentation",
+    redoc_url=None
+)
+
+
+@app.get("/users/", tags=["users"])
+async def get_users():
+    return [{"name": "Harry"}, {"name": "Ron"}]
+
+
+@app.get("/items/", tags=["items"])
+async def get_items():
+    return [{"name": "wand"}, {"name": "flying broom"}]
